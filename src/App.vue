e82f285458941aa971ab587ee7df88e5a00480ff
JAASM1 <alexdroide466@gmail.com>
2025-03-11 13:43:05 -0500

    Menu desplegable

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import Menu from "primevue/menu";
+import { Bars3Icon } from "@heroicons/vue/24/solid";
+
+const router = useRouter();
+const menu = ref(null);
+
+const items = ref([
+  {
+    label: "Opciones",
+    items: [
+      {
+        label: "Eventos",
+        command: () => router.push("/"),
+      },
+      {
+        label: "Crear Evento",
+        command: () => router.push("/crear-evento"),
+      },
+    ],
+  },
+]);
+
+const toggle = (event: Event) => {
+  menu.value?.toggle(event);
+};
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+  <header
+    class="font-poppins bg-Dark text-white px-6 py-4 flex items-center justify-between"
+  >
+    <h1 class="text-3xl font-bold">Evnto</h1>
+    <div>
+      <div class="flex justify-center">
+        <button
+          type="button"
+          @click="toggle($event)"
+          aria-haspopup="true"
+          aria-controls="overlay_menu"
+        >
+          <Bars3Icon class="size-7" />
+        </button>
+        <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+      </div>
     </div>
   </header>
-  <div class="font-poppins">
+  <div class="font-poppins bg-Dark max-md:px-6 min-h-screen">
     <RouterView />
   </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FormCreate from '@/views/Event/FormCreate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path:'/crear-evento',
+      name: 'crear-evento',
+      component:FormCreate
+    }
   ],
 })
 
